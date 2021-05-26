81d8c91376c0a0a80e935ed70a5a698f94571850
Mohamed Aleya <mohamed.aleya@gmail.com>
2021-05-26 16:24:37 +0100

    6. Styling Services Section

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -10,6 +10,7 @@ const GlobalStyle = createGlobalStyle`
 
 body{
     background: #1b1b1b;
+    font-family: 'Inter', sans-serif;
 }
 
 button{
@@ -18,6 +19,7 @@ button{
     cursor: pointer;
     padding: 1rem 2rem;
     border: 3px solid #23d997;
+    font-family: 'Inter', sans-serif;
     background: transparent;
     color: white;
     transition: all 0.5s ease;
