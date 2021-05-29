3064f491cf5a3396441f79d0f18c091676128b5e
Mohamed Aleya <mohamed.aleya@gmail.com>
2021-05-29 02:24:38 +0100

    AboutSection Animation

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -3,44 +3,33 @@ import home1 from "../img/home1.png";
 import { About, Description, Hide, Image } from "../styles";
 //Framer Motion
 import { motion } from "framer-motion";
+import { titleAnim, fade, photoAnim } from "../animation";
 
 const AboutSection = () => {
-  const titleAnim = {
-    hidden: { opacity: 0 },
-    show: { opacity: 11, transition: { duration: 2 } },
-  };
-
-  const container = {
-    hidden: { x: 100 },
-    show: { x: 0 },
-  };
-
   return (
     <About>
       <Description>
-        <div className="title">
+        <motion.div>
           <Hide>
-            <motion.h2 variants={titleAnim} initial="hidden" animate="show">
-              We work to make
+            <motion.h2 variants={titleAnim}>We work to make</motion.h2>
+          </Hide>
+          <Hide>
+            <motion.h2 variants={titleAnim}>
+              your <span>dreams</span> come
             </motion.h2>
           </Hide>
           <Hide>
-            <h2>
-              your <span>dreams</span> come
-            </h2>
+            <motion.h2 variants={titleAnim}>true.</motion.h2>
           </Hide>
-          <Hide>
-            <h2>true.</h2>
-          </Hide>
-        </div>
-        <p>
+        </motion.div>
+        <motion.p variants={fade}>
           Contact us for any photography or videography ideas that you have. We
           have professionals with amazing skills!
-        </p>
-        <button>Contact Us</button>
+        </motion.p>
+        <motion.button variants={fade}>Contact Us</motion.button>
       </Description>
       <Image>
-        <img src={home1} alt="guy with a camera" />
+        <motion.img variants={photoAnim} src={home1} alt="guy with a camera" />
       </Image>
     </About>
   );
